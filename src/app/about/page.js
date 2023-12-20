cd0f349b8f8ece63006c5a318db9b0c1cce9375d
bangaffannn <bangaffannn@gmail.com>
2023-12-20 20:11:53 +0700

    Create about pages

---
+++ src/app/about/page.js
@@ -0,0 +1,11 @@
+export default function About() {
+  return (
+    <div className="max-w-6xl mx-auto space-y-4">
+    <h1 className="text-2xl font-medium text-amber-600">About</h1>
+    <p>Welcome to FlickPulse, where the magic of cinema comes alive at your fingertips! Dive into a world of unparalleled entertainment with our one-stop movie website that caters to cinephiles of all tastes and preferences.</p>
+    <p>
+        Discover:
+        Explore an extensive and diverse collection of movies spanning across genres, decades, and languages. From timeless classics to the latest blockbuster releases, FlickPulse ensures that you're always in the loop with the hottest cinematic trends.</p>
+    </div>
+  )
+}
